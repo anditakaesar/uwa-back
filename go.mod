e12d348195c8e64b49823b1969bbf7cbcdaecae6
anditakaesar <anditakaesar@gmail.com>
2022-09-08 23:55:37 +0700

    change auth using registered user

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.19
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/thoas/go-funk v0.9.2
 	github.com/unrolled/render v1.5.0
 	go.uber.org/zap v1.23.0
+	gorm.io/driver/sqlite v1.3.6
+	gorm.io/gorm v1.23.8
 )
 
 require (
@@ -13,10 +16,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
-	github.com/thoas/go-funk v0.9.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
-	gorm.io/driver/sqlite v1.3.6 // indirect
-	gorm.io/gorm v1.23.8 // indirect
 )
--- services/auth.go
+++ services/auth.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/anditakaesar/uwa-back/application"
+	"github.com/anditakaesar/uwa-back/domain"
+	"github.com/thoas/go-funk"
 )
 
 type AuthParam struct {
@@ -13,17 +15,15 @@ type AuthParam struct {
 }
 
 func AuthUser(appCtx application.Context, authParam AuthParam) error {
-	users := map[string]string{
-		"anditakaesar": "42a9798b99d4afcec9995e47a1d246b98ebc96be7a732323eee39d924006ee1d",
-		"usertwo":      "4a34219b9b4f66a1932428cccae29846b4c5fce07ce7c390b9c5b27e0fea378d",
-	}
+	var user domain.User
+	appCtx.DB.First(&user, "username = ?", authParam.Username)
 
-	authPassHash := users[authParam.Username]
-	if authPassHash == "" {
+	if funk.IsEmpty(user) {
 		appCtx.Log.Warn(fmt.Sprintf("[Services][Auth] auth attempt with user: %s", authParam.Username))
 		return errors.New("[Services][Auth] unauthorized")
 	}
 
+	authPassHash := user.Password
 	ok := appCtx.Crypter.CompareHash(authPassHash, authParam.Password)
 	if !ok {
 		appCtx.Log.Warn(fmt.Sprintf("[Services][Auth] auth attempt with user: %s, pass: %s", authParam.Username, authParam.Password))
